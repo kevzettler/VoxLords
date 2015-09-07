7c71826ec9870768bfb6089a9ac0013e856e8c9e
kevzettler <kevzettler@gmail.com>
2015-09-06 21:02:22 -0700

    no more promise constructors

--- src/entities/Guy.js
+++ src/entities/Guy.js
@@ -3,15 +3,7 @@ const Entity = require('./Entity');
 const THREE = require('three');
 
 function Guy(props){
-    const that = this;
     Guy.super_.call(that,props);
-    // return new Promise((resolve) => {
-    //     Guy.super_.call(that,props).then((guy) => {
-    //             console.log("wtf is guy promise", guy);
-    //             guy.world.client.initPlayerCamera(guy);
-    //             resolve(guy);
-    //     }); 
-    // });
 };
 util.inherits(Guy, Entity);
 
